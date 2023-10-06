9fb39f8469a33f1baf3a6607d6f6ad99d0e9b2bb
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-06 16:03:34 -0400

    spaces

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Liya Getachew 
+ * Created by: Liya Getachew
  * Created on: Oct 2023
  * This program displays all 8 colours in sequential order when button is pressed.
 */
